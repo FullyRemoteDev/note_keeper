dba93ae7d7fa06bf22fe0c0930f30bb280fcfe0b
Jagannath A <jagannath.sysop@gmail.com>
2019-03-18 23:06:11 +0530

    adds note_list initial layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_keeper/screens/note_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,6 +7,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'NoteKeeper',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: NoteList(),
+    );
   }
 }
