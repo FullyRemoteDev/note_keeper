8d5f4e8097c0dc1a42a626efeaa3652f416d109b
Jagannath A <jagannath.sysop@gmail.com>
2019-03-18 23:31:04 +0530

    adds note_detail initial layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_keeper/screens/note_detail.dart';
 import 'package:note_keeper/screens/note_list.dart';
 
 void main() => runApp(MyApp());
